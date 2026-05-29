a44b6cff19aa93bb65db248e6f9faa062718b561
ziyou <youzi_rache@foxmail.com>
2026-05-29 14:04:29 +0800

    update

--- testsuit-for-oskernel/export_ltp_testcode.py
+++ testsuit-for-oskernel/export_ltp_testcode.py
@@ -142,7 +142,6 @@ def render_script(tests, busybox):
         "cd ./ltp/testcases/bin",
         "",
         "export LTPROOT=../..",
-        "PATH=$PATH:/glibc/ltp/testcases/bin",
         "",
         "set -ex",
         "",
